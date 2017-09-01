47a8f73d08de294053d32466bb49ff16ca3470df
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2017-09-01 20:49:05 +0530

    vowel.c

---
+++ vowel.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char n;
+scanf("%c",&n)
+if((n==a||n==e||n==i||n==o||n==u)&&(n==A||n==E||n==I||n==O||n==U))
+printf("Vowel\n");
+else
+printf("Consonant");
+return 0;
+}
